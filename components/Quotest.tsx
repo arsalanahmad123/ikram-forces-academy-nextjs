dffa78006060b95d88cdd6fe98377ef495f1c1f2
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2024-08-24 14:05:58 +0500

    changed navbar

--- components/Quotest.tsx
+++ components/Quotest.tsx
@@ -1,10 +1,10 @@
 export default function Quotes() {
-  return (
-    <div className="py-52 relative inset-0 flex justify-center items-center bg-primary mt-32 dark:bg-gray-900">
-      <h3 className="font-bold lg:text-6xl md:text-5xl text-4xl tracking-wider text-white  max-w-6xl">
-        Building a Brighter Future by Inspiring Every Student to Reach Their
-        Full Potential.
-      </h3>
-    </div>
-  );
+    return (
+        <div className="py-52 relative inset-0 flex justify-center items-center bg-primary mt-32 dark:bg-gray-900">
+            <h3 className="font-bold lg:text-6xl md:text-5xl text-4xl tracking-wider text-white  max-w-6xl text-center lg:text-left">
+                Building a Brighter Future by Inspiring Every Student to Reach
+                Their Full Potential.
+            </h3>
+        </div>
+    );
 }
